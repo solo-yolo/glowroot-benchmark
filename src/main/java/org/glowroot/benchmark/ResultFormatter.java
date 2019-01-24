e9d810daacd85fac9e6c01c070732aed1f651d8e
solo-yolo <sergei.sologub@gmail.com>
2019-01-24 16:34:07 +0200

    refactor result formatter

--- src/main/java/org/glowroot/benchmark/ResultFormatter.java
+++ src/main/java/org/glowroot/benchmark/ResultFormatter.java
@@ -15,9 +15,6 @@
  */
 package org.glowroot.benchmark;
 
-import java.io.File;
-import java.io.IOException;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -25,66 +22,70 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
 
+import java.io.File;
+import java.io.IOException;
+
 public class ResultFormatter {
 
     public static void main(String[] args) throws IOException {
-        File resultFile = new File(args[0]);
+        ArrayNode results = readResults(args[0]);
 
-        Scores scores = new Scores();
-        double baselineStartupTime = -1;
-        double glowrootStartupTime = -1;
+        Score baseline = readScore(results, ServletBenchmark.class.getName(), StartupBenchmark.class.getName());
+        Score glowroot = readScore(results, ServletWithGlowrootBenchmark.class.getName(), StartupWithGlowrootBenchmark.class.getName());
 
+        printScores(baseline, glowroot, "glowroot");
+    }
+
+    private static Score readScore(ArrayNode results, String responseTimeBenchmarkName, String startupBenchmarkName) {
+        Score score = new Score();
+
+        for (JsonNode result : results) {
+            String rawBenchmarkName = result.get("benchmark").asText();
+            String benchmarkName = rawBenchmarkName.substring(0, rawBenchmarkName.lastIndexOf('.'));
+
+            ObjectNode primaryMetric = (ObjectNode) result.get("primaryMetric");
+            double primaryMetricScore = primaryMetric.get("score").asDouble();
+            if (benchmarkName.equals(startupBenchmarkName)) {
+                score.st = primaryMetricScore;
+            } else if (benchmarkName.equals(responseTimeBenchmarkName)) {
+                ObjectNode scorePercentiles = (ObjectNode) primaryMetric.get("scorePercentiles");
+                score.rtAvg = primaryMetricScore;
+                score.rt50 = scorePercentiles.get("50.0").asDouble();
+                score.rt95 = scorePercentiles.get("95.0").asDouble();
+                score.rt99 = scorePercentiles.get("99.0").asDouble();
+                score.rt999 = scorePercentiles.get("99.9").asDouble();
+                score.rt9999 = scorePercentiles.get("99.99").asDouble();
+                score.ab = getAllocatedBytes(result);
+            }
+        }
+
+        return score;
+    }
+
+    private static void printScores(Score baseline, Score apm, String name) {
+        System.out.format("%25s%14s%14s%14s%n", "", "baseline", name, "delta");
+        printLine("Response time (avg)", "µs", baseline.rtAvg, apm.rtAvg, apm.rtAvg - baseline.rtAvg);
+        printLine("Response time (50th)", "µs", baseline.rt50, apm.rt50, apm.rt50 - baseline.rt50);
+        printLine("Response time (95th)", "µs", baseline.rt95, apm.rt95, apm.rt95 - baseline.rt95);
+        printLine("Response time (99th)", "µs", baseline.rt99, apm.rt99, apm.rt99 - baseline.rt99);
+        printLine("Response time (99.9th)", "µs", baseline.rt999, apm.rt999, apm.rt999 - baseline.rt999);
+        printLine("Response time (99.99th)", "µs", baseline.rt9999, apm.rt9999, apm.rt9999 - baseline.rt9999);
+        printLine("Allocation memory per req", "kb", baseline.ab, apm.ab, apm.ab - baseline.ab);
+        printLine("Startup time (avg)", "ms", baseline.st, apm.st, apm.st - baseline.st);
+        System.out.println();
+    }
+
+    private static void printLine(String label, String unit, double baseline, double apm, double delta) {
+        System.out.format("%-25s %10.3f %s %10.3f %s %10.3f %s%n", label, baseline, unit, apm, unit, delta, unit);
+    }
+
+    private static ArrayNode readResults(String pathname) throws IOException {
+        File file = new File(pathname);
         ObjectMapper mapper = new ObjectMapper();
-        String content = Files.toString(resultFile, Charsets.UTF_8);
+        String content = Files.toString(file, Charsets.UTF_8);
         content = content.replaceAll("\n", " ");
         ArrayNode results = (ArrayNode) mapper.readTree(content);
-        for (JsonNode result : results) {
-            String benchmark = result.get("benchmark").asText();
-            benchmark = benchmark.substring(0, benchmark.lastIndexOf('.'));
-            ObjectNode primaryMetric = (ObjectNode) result.get("primaryMetric");
-            double score = primaryMetric.get("score").asDouble();
-            if (benchmark.equals(StartupBenchmark.class.getName())) {
-                baselineStartupTime = score;
-                continue;
-            } else if (benchmark.equals(StartupWithGlowrootBenchmark.class.getName())) {
-                glowrootStartupTime = score;
-                continue;
-            }
-            ObjectNode scorePercentiles = (ObjectNode) primaryMetric.get("scorePercentiles");
-            double score50 = scorePercentiles.get("50.0").asDouble();
-            double score95 = scorePercentiles.get("95.0").asDouble();
-            double score99 = scorePercentiles.get("99.0").asDouble();
-            double score999 = scorePercentiles.get("99.9").asDouble();
-            double score9999 = scorePercentiles.get("99.99").asDouble();
-            double allocatedBytes = getAllocatedBytes(result);
-            if (benchmark.equals(ServletBenchmark.class.getName())) {
-                scores.baselineResponseTimeAvg = score;
-                scores.baselineResponseTime50 = score50;
-                scores.baselineResponseTime95 = score95;
-                scores.baselineResponseTime99 = score99;
-                scores.baselineResponseTime999 = score999;
-                scores.baselineResponseTime9999 = score9999;
-                scores.baselineAllocatedBytes = allocatedBytes;
-            } else if (benchmark.equals(ServletWithGlowrootBenchmark.class.getName())) {
-                scores.glowrootResponseTimeAvg = score;
-                scores.glowrootResponseTime50 = score50;
-                scores.glowrootResponseTime95 = score95;
-                scores.glowrootResponseTime99 = score99;
-                scores.glowrootResponseTime999 = score999;
-                scores.glowrootResponseTime9999 = score9999;
-                scores.glowrootAllocatedBytes = allocatedBytes;
-            } else {
-                throw new AssertionError(benchmark);
-            }
-        }
-        System.out.println();
-        printScores(scores);
-        if (baselineStartupTime != -1) {
-            System.out.println("STARTUP");
-            System.out.format("%25s%14s%14s%n", "", "baseline", "glowroot");
-            printLine("Startup time (avg)", "ms", baselineStartupTime, glowrootStartupTime);
-        }
-        System.out.println();
+        return results;
     }
 
     private static double getAllocatedBytes(JsonNode result) {
@@ -99,47 +100,15 @@ public class ResultFormatter {
         return gcAllocRateNorm.get("score").asDouble();
     }
 
-    private static void printScores(Scores scores) {
-        System.out.format("%25s%14s%14s%n", "", "baseline", "glowroot");
-        printLine("Response time (avg)", "us", scores.baselineResponseTimeAvg,
-                scores.glowrootResponseTimeAvg);
-        printLine("Response time (50th)", "us", scores.baselineResponseTime50,
-                scores.glowrootResponseTime50);
-        printLine("Response time (95th)", "us", scores.baselineResponseTime95,
-                scores.glowrootResponseTime95);
-        printLine("Response time (99th)", "us", scores.baselineResponseTime99,
-                scores.glowrootResponseTime99);
-        printLine("Response time (99.9th)", "us", scores.baselineResponseTime999,
-                scores.glowrootResponseTime999);
-        printLine("Response time (99.99th)", "us", scores.baselineResponseTime9999,
-                scores.glowrootResponseTime9999);
-        if (scores.baselineAllocatedBytes != -1 && scores.glowrootAllocatedBytes != -1) {
-            printLine("Allocation memory per req", "kb", scores.baselineAllocatedBytes / 1024.0,
-                    scores.glowrootAllocatedBytes / 1024.0);
-        }
-        System.out.println();
+    private static class Score {
+        private double rtAvg;
+        private double rt50;
+        private double rt95;
+        private double rt99;
+        private double rt999;
+        private double rt9999;
+        private double ab;
+        private double st;
     }
 
-    private static void printLine(String label, String unit, double baselineScore,
-            double glowrootScore) {
-        System.out.format("%-25s %10.3f %s %10.3f %s%n", label, baselineScore, unit, glowrootScore,
-                unit);
-    }
-
-    private static class Scores {
-        private double baselineResponseTimeAvg;
-        private double baselineResponseTime50;
-        private double baselineResponseTime95;
-        private double baselineResponseTime99;
-        private double baselineResponseTime999;
-        private double baselineResponseTime9999;
-        private double baselineAllocatedBytes;
-        private double glowrootResponseTimeAvg;
-        private double glowrootResponseTime50;
-        private double glowrootResponseTime95;
-        private double glowrootResponseTime99;
-        private double glowrootResponseTime999;
-        private double glowrootResponseTime9999;
-        private double glowrootAllocatedBytes;
-    }
 }
