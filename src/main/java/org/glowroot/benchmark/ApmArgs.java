f5e69f5b7d941fe86e7e9c246802c02d51311d70
solo-yolo <sergei.sologub@gmail.com>
2019-01-28 12:02:19 +0200

    add stackify retrace benchmarks

--- src/main/java/org/glowroot/benchmark/ApmArgs.java
+++ src/main/java/org/glowroot/benchmark/ApmArgs.java
@@ -4,5 +4,5 @@ public interface ApmArgs {
 
     String GLOWROOT_AGENT = "-javaagent:glowroot/glowroot.jar";
     String NEWRELIC_AGENT = "-javaagent:newrelic/newrelic.jar";
-
+    String STACKIFY_AGENT = "-javaagent:stackify/stackify-java-apm.jar";
 }
